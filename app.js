37aec996487030bbcf7cae1d452b6398b8d25c80
pruthviraj Yadav <114584082+Pruthviraj002@users.noreply.github.com>
2024-10-03 20:13:10 +0530

    teacher-crud

---
+++ app.js
@@ -0,0 +1,31 @@
+const Teachers = require("./Route/techRoute")
+const StaffRoute = require("./Route/staffRoute")
+const mongoose = require("mongoose")
+const express = require("express")
+require("dotenv/config")
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server")
+})
+
+app.use("/api/teachers",Teachers)
+app.use("/api/staff",StaffRoute)
+
+app.listen(process.env.PORT || 5002)
+ async function db() {
+    try {
+        const res = await  mongoose.connect(process.env.DB)
+        const data = await res.default
+        console.log(data.STATES.connected);
+        
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+ }
+
+ db()
